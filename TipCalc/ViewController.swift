a9dac0ddc59cef0dd417d9f7d0c7055534b36903
Dean <dean.pektas27@qmail.cuny.edu>
2019-01-11 22:39:31 -0500

    Changes

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -51,6 +51,31 @@ class ViewController: UIViewController {
     }
     
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        tipOptions.selectedSegmentIndex = defaults.integer(forKey: "myInt")
+        // and use it to update the tip amount
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did disappear")
+    }
+    
+    
     
 
     override func didReceiveMemoryWarning() {
